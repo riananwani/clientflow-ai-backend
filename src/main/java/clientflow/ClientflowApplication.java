012f9025bc727d990398e9d82b4bcb8251f04331
Ria Nanwani <riananwani1510@gmail.com>
2026-09-01 16:05:09 +0530

    Setup + Endpoint

---
+++ src/main/java/clientflow/ClientflowApplication.java
@@ -0,0 +1,13 @@
+package clientflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientflowApplication.class, args);
+	}
+
+}
